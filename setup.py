f39facd05dda0ab09312deb9bfa08adeb2e7689e
Mohd Rozi <blackrosezy@gmail.com>
2016-01-22 21:43:14 +1000

    python packaging is working

--- setup.py
+++ setup.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
         name='Haproxyctl',
         version='0.1.0',
         author='Mohd Rozi',
         author_email='blackrosezy@gmail.com',
-        scripts=['bin/haproxyctl'],
+        packages=['haproxyctl'],
         description='Haproxy reverse proxy cli',
         install_requires=[
             "Jinja2>=2.8",
             "docker-py>=1.6.0",
             "docopt>=0.6.2",
         ],
+        include_package_data=True,
+        entry_points={
+            'console_scripts': [
+                'haproxyctl=haproxyctl.haproxyctl:main',
+            ],
+        },
 )
